b00882879cc116cd3373561470e9f56cc2c3ca9a
yanfeng <winnelc@126.com>
2023-07-26 13:40:21 +0000

    add clang tool "

---
+++ main.cc
@@ -0,0 +1,18 @@
+static llvm::cl::OptionCategory MatcherCategory("Matcher");
+
+
+int main(int argc, const char **argv) {
+  google::InitGoogleLogging(argv[0]);
+
+  auto ExpectedParser = CommonOptionsParser::create(argc, argv, MatcherCategory);
+  if (!ExpectedParser) {
+    // Fail gracefully for unsupported options.
+    std::cerr << "ExpectedParser error, return" << std::endl;
+    return 1;
+  }
+
+  CommonOptionsParser& op = ExpectedParser.get();
+  ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+
+  Tool.run(newFrontendActionFactory<ConvertAction>().get());
+}
