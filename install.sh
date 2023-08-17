9601071d87ae89f631c1428edcc96421c07e90b4
yanfeng <yanfeng.03@bytedance.com>
2023-08-17 22:26:55 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+# 按照 vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+# 安装 tig
+git clont https://github.com/jonas/tig.git
