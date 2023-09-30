9e676af932c579190580dc26e92568262ece8e43
yanfeng <winnelc@126.com>
2023-09-30 20:27:51 +0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 static llvm::cl::OptionCategory MatcherCategory("Matcher");
 
@@ -17,4 +18,31 @@ int main(int argc, const char **argv) {
   ClangTool Tool(op.getCompilations(), op.getSourcePathList());
 
   Tool.run(newFrontendActionFactory<ConvertAction>().get());
+}
+*/
+
+#include <iostream>
+#include <type_traits>
+
+// 检查类是否具有名为 `value` 的成员变量
+template<typename T, typename = void>
+struct has_value_member : std::false_type {};
+
+template<typename T>
+struct has_value_member<T, std::void_t<decltype(std::declval<T>().value)>> : std::true_type {};
+
+// 示例类
+struct A {
+    int value;
+};
+
+struct B {
+    float other_value;
+};
+
+int main() {
+    std::cout << std::boolalpha;
+    std::cout << "A has value member: " << has_value_member<A>::value << std::endl; // 输出 true
+    std::cout << "B has value member: " << has_value_member<B>::value << std::endl; // 输出 false
+    return 0;
 }
