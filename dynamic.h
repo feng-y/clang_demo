2753f85d4a709af79f9cbea6e8acd815129dd9ef
yanfeng <yanfeng.03@bytedance.com>
2024-08-22 13:58:45 +0800

    add DynamicWorker

--- dynamic.h
+++ dynamic.h
@@ -1,3 +1,12 @@
 #pragma once
 
 #include <string>
+
+class DynamicWorker {
+public:
+    DynamicWorker() = default;
+    DynamicWorker(bool verbose) : verbose_(verbose) {}
+
+private:
+    bool verbose_ = true;
+};
